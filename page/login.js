af5f684e4675ca26aed36f307abe05e400fe620d
xipins <pinayulyanti7@gmail.com>
2023-10-27 14:41:56 +0700

    push

--- page/login.js
+++ page/login.js
@@ -1,23 +1,23 @@
 const user = {
-    username : "aryalesmana",
-    password : "arya1008"
+    username: "aryalesmana",
+    password: "arya1008"
 }
 
 
-function login(){
-    const username =document.getElementById("username")
+function login() {
+    const username = document.getElementById("username")
     const password = document.getElementById("password")
-    if(username.value === "" || password.value === ""){
+    if (username.value === "" || password.value === "") {
         window.alert("username dan password harus di isi");
-    }else if(
-        username.value != user.username||
+    } else if (
+        username.value != user.username ||
         password.value != user.password
-    ){
+    ) {
         window.alert("username dan password tidak sesuai")
-    }else{
-        location.href= "menu.html"
+    } else {
+        location.href = "menu.html"
     }
     username.value = "";
-    password.value = "";    
+    password.value = "";
 
 }
